05589c40bb983b04639d6b997870711c334f2929
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-27 14:24:52 -0400

    Update get-juiced-data.R

--- R/get-juiced-data.R
+++ R/get-juiced-data.R
@@ -5,8 +5,11 @@
 #' @author Steven P. Sanderson II, MPH
 #'
 #' @description
+#' This is a simple function that will get the juiced data from a recipe.
 #'
 #' @details
+#' Instead of typing out something like:
+#'   \code {recipe_object %>% prep() %>% juice() %>% glimpse()}
 #'
 #' @param
 #'
