0031e7d72bf1e486bde9e1299afcc78b9f6fe5df
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-08-31 14:58:11 -0400

    Create auto-kmeans.R

---
+++ R/auto-kmeans.R
@@ -0,0 +1,20 @@
+#' Automatic K-Means H2O
+#'
+#' @family Clustering
+#' @family Kmeans
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @description
+#'
+#' @details
+#'
+#' @param
+#'
+#' @examples
+#'
+#' @return
+#'
+#' @export
+#'
+
