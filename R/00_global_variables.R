fb94fc31c1d8d470fbd8e4145b6fc46bbf5c2365
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-09-02 14:46:26 -0400

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -2,6 +2,7 @@ globalVariables(
     names = c(
         "outside",".cluster","centers","glance","k_means","prop_of_total","tot.withinss",
         "total_records","PC","var_explained","cum_var_pct","ou_threshold",
-        "iterations","number_of_clusters","within_cluster_sum_of_squares","wss"
+        "iterations","number_of_clusters","within_cluster_sum_of_squares","wss",
+        "predict","predicted_cluster"
     )
 )
