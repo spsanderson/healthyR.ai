2d5e12cb69054fe048d271bb844f3b452b03fbc3
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-09-02 14:09:06 -0400

    Create auto-kmeans-predict.R

---
+++ R/auto-kmeans-predict.R
@@ -0,0 +1,27 @@
+#' Automatic K-Means H2O
+#'
+#' @family Kmeans
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @description
+#' This is a wrapper around the [h2o::h2o.predict()] function that will return a list
+#' object with a lot of useful and easy to use tidy style information.
+#'
+#' @param .input This is the output of the [healthyR.ai::hai_kmeans_automl()] function.
+#'
+#' @examples
+#' \dontrun{
+#' h2o.init()
+#' output <- hai_kmeans_automl(
+#'     .data = iris,
+#'     .predictors = c("Sepal.Width","Sepal.Length","Petal.Width","Petal.Length"),
+#'     .standardize = FALSE
+#' )
+#' h2o.shutdown()
+#' }
+#'
+#' @return A list object
+#'
+#' @export
+#'
