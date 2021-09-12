bec9d3ef6d8a53f0aa978a9b6fb59d1924f596dc
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-09-11 21:47:29 -0400

    Create pre-proc-data.R

---
+++ R/pre-proc-data.R
@@ -0,0 +1,50 @@
+#' Data Preprocessor
+#'
+#' @family Data Wrangling
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @description
+#' Takes in a data.frame/tibble and transforms it into a processed data.frame/tibble.
+#'
+#' @details
+#' This function will get your data ready for processing with many types of ml/ai
+#' models. This is an R translation from the PyCaret pre-processor.
+#'
+#' @seealso \url{https://pycaret.org/preprocessing/}
+#'
+#' @param .data The data that you want to process
+#'
+#' @examples
+#' library(healthyR.data)
+#' library(dplyr)
+#'
+#' data_tbl <- healthyR_data%>%
+#'    filter(ip_op_flag == "I") %>%
+#'    filter(payer_grouping != "Medicare B") %>%
+#'    filter(payer_grouping != "?") %>%
+#'    select(service_line, payer_grouping) %>%
+#'    mutate(record = 1) %>%
+#'    as_tibble()
+#'
+#' @return
+#' A processed data.frame/tibble.
+#'
+#' @export
+#'
+
+preprocess_your_data <- function(.data){
+
+    # * Tidyeval ----
+
+    # * Checks ----
+    if(!is.data.frame(.data)){
+        stop(call. = FALSE, "(.data) must be a data.frame/tibble.")
+    }
+
+    # * Manipulation ----
+    data_tbl <- tibble::as_tibble(.data)
+
+    # * Return ----
+    return(data_tbl)
+}
