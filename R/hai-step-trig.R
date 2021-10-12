75742d841093c19ab794e652bcb9a4439a1f97df
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2021-10-12 14:28:59 -0400

    Create hai-step-trig.R

---
+++ R/hai-step-trig.R
@@ -0,0 +1,20 @@
+#' Data Preprocessor - Step Trigonometry
+#'
+#' @family Data Recipes
+#' @family Preprocessor
+#'
+#' @keywords internal
+#'
+#' @author Steven P. Sanderson II, MPH
+#'
+#' @description
+#' Takes in a recipe and will add sin, cos and tan transformed values. This function
+#' is not exported but may be called via the ::: method.
+#'
+#' @details
+#' This function will get your data ready for processing with many types of ml/ai
+#' models.
+#'
+#' This is intended to be used inside of the [healthyR.ai::hai_data_preprocessor()] and
+#' therefore is an internal function. This documentation exists to explain the process
+#' and help the user understand the parameters that can be set in the pre-processor function.
