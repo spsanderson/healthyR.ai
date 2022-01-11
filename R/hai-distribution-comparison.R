94d3919a92858b34eff8abf2df641ca2464cd759
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-11 14:55:23 -0500

    Update hai-distribution-comparison.R

--- R/hai-distribution-comparison.R
+++ R/hai-distribution-comparison.R
@@ -3,7 +3,14 @@
 #' @author Steven P. Sanderson II, MPH
 #'
 #' @details Get information on the empirical distribution of your data along with
-#' generated densities of other distributions.
+#' generated densities of other distributions. This information is in the resulting
+#' tibble that is generated. Three columns will generate, Distribution, from the
+#' `param .distributions`, `dist_data` which is a list vector of density
+#' values passed to the underlying stats r distribution function, and `density_data`,
+#' which is the `dist_data` column passed to `list(stats::density(unlist(dist_data)))`
+#'
+#' This has the effect of giving you the desired vector that can be used in resultant
+#' plots (`dist_data`) or you can interact with the `density` object itself.
 #'
 #' @description This function will attempt to get some key information on the
 #' data you pass to it. It will also automatically normalize the data from 0 to 1.
@@ -11,13 +18,46 @@
 #' that many different types of distributions can be fit to the data, which
 #' should help identify what the distribution of the passed data could be.
 #'
-#' @seealso
+#' The resulting output has a class of `hai_density_tbl` attached to it, if you
+#' want to interact with it in the typicall `tidyverse` fashion, simply pipe it
+#' to `dplyr::as_tibble` and continue.
+#'
+#' This function will automatically pass the `.x` parameter to [healthyR.ai::hai_skewness_vec()]
+#' and [healthyR.ai::hai_kurtosis_vec()] in order to help create the random data
+#' from the distributions.
+#'
+#' The distributions that can be chosen from are:
+#'
+#' | Distribution | R stats::dist |
+#' | --- | --- |
+#' | normal | rnorm |
+#' | uniform | runif |
+#' | exponential | rexp |
+#' | logistic | rlogis |
+#' | beta | rbeta |
+#' | lognormal | rlnorm |
+#' | gamma | rgamma |
+#' | weibull | weibull |
+#' | chisquare | rchisq |
+#' | cauchy | rcauchy |
+#' | hypergeometric | rhyper |
+#' | f | rf |
+#' | poisson | rpois |
 #'
 #' @param .x The numeric vector to analyze.
+#' @param .distributions A character vector of distributions to check. For example,
+#' c("gamma","beta")
 #'
 #' @examples
+#' x_vec <- hai_scale_zero_one_vec(mtcars$mpg)
+#' df <- hai_distribution_comparison_tbl(
+#'   .x = x_vec,
+#'   .distributions = c("beta","gamma")
+#' )
+#' df
 #'
 #' @return
+#' A tibble.
 #'
 #' @export
 #'
@@ -82,32 +122,29 @@ hai_distribution_comparison_tbl <- function(.x, .distributions = c("gamma","beta
         dplyr::mutate(
             dist_data = dplyr::case_when(
                 stats_func == "rgamma" ~ list(stats::rgamma(n = n, shape = hskew, rate = hkurt)),
-                stats_func == "rbeta"  ~ list(stats::rbeta(n = n, shape1 = hskew, shape2 = hkurt)),
-                stats_func == "rnorm"  ~ list(stats::rnorm(n = n, mean = mu, sd = std)),
-                stats_func == "runif"  ~ list(stats::runif(n = n, min = minimum, max = maximum)),
-                stats_func == "rexp"   ~ list(stats::rexp(n = n, rate = hkurt)),
+                stats_func == "rbeta" ~ list(stats::rbeta(n = n, shape1 = hskew, shape2 = hkurt, ncp = med)),
+                stats_func == "rnorm" ~ list(stats::rnorm(n = n, mean = mu, sd = std)),
+                stats_func == "runif" ~ list(stats::runif(n = n, min = minimum, max = maximum)),
+                stats_func == "rexp" ~ list(stats::rexp(n = n, rate = hkurt)),
                 stats_func == "rlogis" ~ list(stats::rlogis(n = n, location = hskew, scale = hkurt)),
                 stats_func == "rlnorm" ~ list(stats::rlnorm(n = n, meanlog = log(mu), sdlog = log(std))),
                 stats_func == "rweibull" ~ list(stats::rweibull(n = n, shape = hskew, scale = hkurt)),
-                stats_func == "rchisq"   ~ list(stats::rchisq(n = n, df = hskew)),
-                stats_func == "rcauchy"  ~ list(stats::rcauchy(n = n, location = hskew, scale = hkurt)),
-                stats_func == "rhyper"   ~ list(stats::rhyper(nn = n, m = n, n = n, k = n) %>%
-                                                  healthyR.ai::hai_scale_zero_one_vec()),
-                stats_func == "rf"       ~ list(stats::rf(n = n, df1 = hskew, df2 = hskew) %>%
-                                              healthyR.ai::hai_scale_zero_one_vec()),
-                stats_func == "rpois"    ~ list(stats::rpois(n = n, lambda = hskew) %>%
-                                                 healthyR.ai::hai_scale_zero_one_vec())
+                stats_func == "rchisq" ~ list(stats::rchisq(n = n, df = hskew)),
+                stats_func == "rcauchy" ~ list(stats::rcauchy(n = n, location = hskew, scale = hkurt)),
+                stats_func == "rhyper" ~ list(stats::rhyper(nn = n, m = n, n = n, k = n)),
+                stats_func == "rf" ~ list(stats::rf(n = n, df1 = hskew, df2 = hskew)),
+                stats_func == "rpois" ~ list(stats::rpois(n = n, lambda = hskew))
             )
         ) %>%
         dplyr::group_by(distribution) %>%
-        dplyr::mutate(density_data = list(density(unlist(dist_data)))) %>%
+        dplyr::mutate(density_data = list(stats::density(unlist(dist_data)))) %>%
         dplyr::ungroup() %>%
         dplyr::select(-stats_func)
 
     # Add empirical data and density to tibble
     emp_dens_tbl <- tibble::tibble(
         distribution = "empirical",
-        dist_data    = list(x_term),
+        dist_data = list(x_term),
         density_data = list(density(x_term))
     )
 
