35a4b96d3eb69d08a00202852c122bb6de1fd3ce
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-12 11:44:16 -0500

    Update 00_global_variables.R

--- R/00_global_variables.R
+++ R/00_global_variables.R
@@ -3,6 +3,8 @@ globalVariables(
         "outside",".cluster","centers","glance","k_means","prop_of_total","tot.withinss",
         "total_records","PC","var_explained","cum_var_pct","ou_threshold",
         "iterations","number_of_clusters","within_cluster_sum_of_squares","wss",
-        "predict","predicted_cluster","printer","rand_id"
+        "predict","predicted_cluster","printer","rand_id",
+        "distribution","dist_data","density","density_data","density_obj","df",
+        "dist_supported","stats_func","value","x","y"
     )
 )
