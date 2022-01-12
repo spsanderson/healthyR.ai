b05d7747248cb3ceb463f5638b748995a77751e5
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-01-12 10:26:34 -0500

    Update tbl-hai-get-density-data.R

--- R/tbl-hai-get-density-data.R
+++ R/tbl-hai-get-density-data.R
@@ -5,18 +5,17 @@
 #' @author Steven P. Sanderson II, MPH
 #'
 #' @details This function expects to take the output of the `hai_distribution_comparison_tbl()`
-#' function. It returns a tibble of the distribution and the randomly generated
-#' data produced from the associated stats r function like `rnorm`
+#' function. It returns a tibble of the `tidy` density data.
 #'
 #' @description This function will return a tibble that can either be nested/unnested,
-#' and grouped or ungrouped. The `.data` argument must be the output of the
+#' and grouped or un-grouped. The `.data` argument must be the output of the
 #' `hai_distribution_comparison_tbl()` function.
 #'
 #' @param .data The data from the `hai_distribution_comparison_tbl()` function
-#' as this function looks for a class of 'hai_dist_data'
-#' @param .unnest Should the resulting tibble be unnested, a boolean value TRUE/FALSE.
+#' as this function looks for an attribute of `hai_dist_compare_tbl`
+#' @param .unnest Should the resulting tibble be un-nested, a Boolean value TRUE/FALSE.
 #' The default is TRUE
-#' @param .group_data Shold the resulting tibble be grouped, a boolean value TRUE/FALSE.
+#' @param .group_data Should the resulting tibble be grouped, a Boolean value TRUE/FALSE.
 #' The default is FALSE
 #'
 #' @examples
@@ -24,7 +23,7 @@
 #'
 #' df <- hai_scale_zero_one_vec(.x = mtcars$mpg) %>%
 #'   hai_distribution_comparison_tbl()
-#' hai_get_dist_data_tbl(df)
+#' hai_get_density_data_tbl(df)
 #'
 #' @return
 #' A tibble.
