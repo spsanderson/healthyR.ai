33143697e86feb1e797dd16d776a74a99c1c54e1
Steven Paul Sanderson II, MPH <spsanderson@users.noreply.github.com>
2022-06-02 11:25:26 -0400

    update site

--- R/data-prep-glmnet.R
+++ R/data-prep-glmnet.R
@@ -21,7 +21,7 @@
 #'
 #' @examples
 #' hai_glmnet_data_prepper(.data = Titanic, .recipe_formula = Survived ~ .)
-#' rec_obj <- hai_glmnet_data_prepper(Survived ~ ., Titanic)
+#' rec_obj <- hai_glmnet_data_prepper(Titanic, Survived ~ .)
 #' get_juiced_data(rec_obj)
 #'
 #' @return
